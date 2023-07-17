7b9a64354c9c7e78c586fd3867df76efd552e33e
themanmaran <tyler.maran@gmail.com>
2023-07-17 07:20:03 -0400

    meta image

--- iris/app/layout.tsx
+++ iris/app/layout.tsx
@@ -6,7 +6,25 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Iris Glass',
-	description: 'Next generation wearable technology',
+	description: 'Seeing is believing',
+	openGraph: {
+		type: 'website',
+		url: 'https://iris-glass.netlify.app/',
+		title: 'Iris Glass',
+		description: 'Seeing is believing',
+		siteName: 'Iris',
+		images: [
+			{
+				url: 'https://iris-glass.netlify.app/meta.png',
+			},
+		],
+	},
+	twitter: {
+		card: 'summary_large_image',
+		site: 'Iris Glass',
+		creator: 'Iris',
+		images: 'https://iris-glass.netlify.app/meta.png',
+	},
 };
 
 export default function RootLayout({
